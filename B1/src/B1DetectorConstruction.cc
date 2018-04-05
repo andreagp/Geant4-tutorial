2b725f86d2ea07ec2fcee45832386a0ff7d64e3d
tsbita <tsbita91@gmail.com>
2018-04-05 12:04:09 +0200

    new collimator

--- B1/src/B1DetectorConstruction.cc
+++ B1/src/B1DetectorConstruction.cc
@@ -40,6 +40,7 @@
 #include "G4LogicalVolume.hh"
 #include "G4PVPlacement.hh"
 #include "G4SystemOfUnits.hh"
+#include "G4Tubs.hh"
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
@@ -62,7 +63,7 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
   
   // Envelope parameters
   //
-  G4double env_sizeXY = 20*cm, env_sizeZ = 30*cm;
+  G4double env_sizeXY = 20*cm, env_sizeZ = 40*cm;
   G4Material* env_mat = nist->FindOrBuildMaterial("G4_WATER");
    
   // Option to switch on/off checking of volumes overlaps
@@ -120,9 +121,10 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
   // Shape 1
   //  
   G4Material* shape1_mat = nist->FindOrBuildMaterial("G4_A-150_TISSUE");
-  G4ThreeVector pos1 = G4ThreeVector(0, 2*cm, -7*cm);
+  G4ThreeVector pos1 = G4ThreeVector(0, 0*cm, -12*cm);
         
-  // Conical section shape       
+  // Conical section shape
+  /*  
   G4double shape1_rmina =  0.*cm, shape1_rmaxa = 2.*cm;
   G4double shape1_rminb =  0.*cm, shape1_rmaxb = 4.*cm;
   G4double shape1_hz = 3.*cm;
@@ -131,7 +133,19 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
     new G4Cons("Shape1", 
     shape1_rmina, shape1_rmaxa, shape1_rminb, shape1_rmaxb, shape1_hz,
     shape1_phimin, shape1_phimax);
-                      
+  
+  */
+  //changed shape1 to G4Tubs cylinder
+  
+  G4double R1 = 0. *cm;
+  G4double R2 = 3. *cm;
+  G4double hz = 8. *cm;
+  G4double stdeg = 0. *deg;
+  G4double spanningangle = 360. *deg;
+  
+  G4Tubs* solidShape1 =
+    new G4Tubs("tracker1", R1, R2, hz, stdeg, spanningangle);
+   
   G4LogicalVolume* logicShape1 =                         
     new G4LogicalVolume(solidShape1,         //its solid
                         shape1_mat,          //its material
@@ -145,13 +159,22 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
                     false,                   //no boolean operation
                     0,                       //copy number
                     checkOverlaps);          //overlaps checking
-
+  
   //     
   // Shape 2
   //
   G4Material* shape2_mat = nist->FindOrBuildMaterial("G4_BONE_COMPACT_ICRU");
-  G4ThreeVector pos2 = G4ThreeVector(0, -1*cm, 7*cm);
+  G4ThreeVector pos2 = G4ThreeVector(0, 0*cm, 10*cm);
 
+  //create detector wall
+  G4double det_x = 12*cm;
+  G4double det_y = 12*cm;
+  G4double det_z = 2*cm;
+
+  G4Box* detwall =
+    new G4Box("detwall", //its name
+	      0.5*det_x, 0.5*det_y, 0.5*det_z); //its size
+  /*
   // Trapezoid shape       
   G4double shape2_dxa = 12*cm, shape2_dxb = 12*cm;
   G4double shape2_dya = 10*cm, shape2_dyb = 16*cm;
@@ -160,24 +183,243 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
     new G4Trd("Shape2",                      //its name
               0.5*shape2_dxa, 0.5*shape2_dxb, 
               0.5*shape2_dya, 0.5*shape2_dyb, 0.5*shape2_dz); //its size
-                
-  G4LogicalVolume* logicShape2 =                         
-    new G4LogicalVolume(solidShape2,         //its solid
+  */           
+  G4LogicalVolume* logicdetwall =                         
+    new G4LogicalVolume(detwall,         //its solid
                         shape2_mat,          //its material
                         "Shape2");           //its name
                
   new G4PVPlacement(0,                       //no rotation
                     pos2,                    //at position
-                    logicShape2,             //its logical volume
+                    logicdetwall,             //its logical volume
                     "Shape2",                //its name
                     logicEnv,                //its mother  volume
                     false,                   //no boolean operation
                     0,                       //copy number
                     checkOverlaps);          //overlaps checking
-                
+
+  //add collimator blades on detector wall
+  // first collimator blade
+
+  G4ThreeVector pos3 = G4ThreeVector(0, 0*cm, 7*cm);
+  G4RotationMatrix *rotm1  = new G4RotationMatrix();
+  rotm1->rotateZ(14.*deg);
+  rotm1->rotateX(40.*deg);
+
+  
+  G4double det_xc1 = 0.01*cm;
+  G4double det_yc1 = 12*cm;
+  G4double det_zc1 = 4*cm;
+
+  G4Box* detcol1 =
+    new G4Box("col1", //its name
+	      0.5*det_xc1, 0.5*det_yc1, 0.5*det_zc1); //its size
+  
+   G4LogicalVolume* logiccol1 =                         
+    new G4LogicalVolume(detcol1,         //its solid
+                        shape2_mat,          //its material
+                        "Shape2");           //its name
+               
+  new G4PVPlacement(rotm1,                       //no rotation
+                    pos3,                    //at position
+                    logiccol1,             //its logical volume
+                    "Shape2",                //its name
+                    logicEnv,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+  // second collimator blade
+  G4ThreeVector pos4 = G4ThreeVector(6*cm, 0*cm, 7*cm);
+  
+  G4Box* detcol2 =
+    new G4Box("col2", //its name
+	      0.5*det_xc1, 0.5*det_yc1, 0.5*det_zc1); //its size
+  
+   G4LogicalVolume* logiccol2 =                         
+    new G4LogicalVolume(detcol2,         //its solid
+                        shape2_mat,          //its material
+                        "Shape2");           //its name
+               
+  new G4PVPlacement(0,                       //no rotation
+                    pos4,                    //at position
+                    logiccol2,             //its logical volume
+                    "Shape2",                //its name
+                    logicEnv,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+    // 3rd  collimator blade
+  G4ThreeVector pos5 = G4ThreeVector(-6*cm, 0*cm, 7*cm);
+  
+  G4Box* detcol3 =
+    new G4Box("col3", //its name
+	      0.5*det_xc1, 0.5*det_yc1, 0.5*det_zc1); //its size
+  
+   G4LogicalVolume* logiccol3 =                         
+    new G4LogicalVolume(detcol3,         //its solid
+                        shape2_mat,          //its material
+                        "Shape2");           //its name
+               
+  new G4PVPlacement(0,                       //no rotation
+                    pos5,                    //at position
+                    logiccol3,             //its logical volume
+                    "Shape2",                //its name
+                    logicEnv,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+    // 4th collimator blade
+  G4ThreeVector pos6 = G4ThreeVector(3*cm, 0*cm, 7*cm);
+  
+  G4Box* detcol4 =
+    new G4Box("col4", //its name
+	      0.5*det_xc1, 0.5*det_yc1, 0.5*det_zc1); //its size
+  
+   G4LogicalVolume* logiccol4 =                         
+    new G4LogicalVolume(detcol4,         //its solid
+                        shape2_mat,          //its material
+                        "Shape2");           //its name
+               
+  new G4PVPlacement(0,                       //no rotation
+                    pos6,                    //at position
+                    logiccol4,             //its logical volume
+                    "Shape2",                //its name
+                    logicEnv,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+      // 5th collimator blade
+  G4ThreeVector pos7 = G4ThreeVector(-3*cm, 0*cm, 7*cm);
+  
+  G4Box* detcol5 =
+    new G4Box("col5", //its name
+	      0.5*det_xc1, 0.5*det_yc1, 0.5*det_zc1); //its size
+  
+   G4LogicalVolume* logiccol5 =                         
+    new G4LogicalVolume(detcol5,         //its solid
+                        shape2_mat,          //its material
+                        "Shape2");           //its name
+               
+  new G4PVPlacement(0,                       //no rotation
+                    pos7,                    //at position
+                    logiccol5,             //its logical volume
+                    "Shape2",                //its name
+                    logicEnv,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+      // 6th collimator blade
+  G4ThreeVector pos8 = G4ThreeVector(1.5*cm, 0*cm, 7*cm);
+  
+  G4Box* detcol6 =
+    new G4Box("col6", //its name
+	      0.5*det_xc1, 0.5*det_yc1, 0.5*det_zc1); //its size
+  
+   G4LogicalVolume* logiccol6 =                         
+    new G4LogicalVolume(detcol6,         //its solid
+                        shape2_mat,          //its material
+                        "Shape2");           //its name
+               
+  new G4PVPlacement(0,                       //no rotation
+                    pos8,                    //at position
+                    logiccol6,             //its logical volume
+                    "Shape2",                //its name
+                    logicEnv,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+      // 7th collimator blade
+  G4ThreeVector pos9 = G4ThreeVector(-1.5*cm, 0*cm, 7*cm);
+  
+  G4Box* detcol7 =
+    new G4Box("col7", //its name
+	      0.5*det_xc1, 0.5*det_yc1, 0.5*det_zc1); //its size
+  
+   G4LogicalVolume* logiccol7 =                         
+    new G4LogicalVolume(detcol7,         //its solid
+                        shape2_mat,          //its material
+                        "Shape2");           //its name
+               
+  new G4PVPlacement(0,                       //no rotation
+                    pos9,                    //at position
+                    logiccol7,             //its logical volume
+                    "Shape2",                //its name
+                    logicEnv,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+      // 8th collimator blade
+  G4ThreeVector pos10 = G4ThreeVector(4.5*cm, 0*cm, 7*cm);
+  
+  G4Box* detcol8 =
+    new G4Box("col8", //its name
+	      0.5*det_xc1, 0.5*det_yc1, 0.5*det_zc1); //its size
+  
+   G4LogicalVolume* logiccol8 =                         
+    new G4LogicalVolume(detcol8,         //its solid
+                        shape2_mat,          //its material
+                        "Shape2");           //its name
+               
+  new G4PVPlacement(0,                       //no rotation
+                    pos10,                    //at position
+                    logiccol8,             //its logical volume
+                    "Shape2",                //its name
+                    logicEnv,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+      // 9th collimator blade
+  G4ThreeVector pos11 = G4ThreeVector(-4.5*cm, 0*cm, 7*cm);
+  
+  G4Box* detcol9 =
+    new G4Box("col9", //its name
+	      0.5*det_xc1, 0.5*det_yc1, 0.5*det_zc1); //its size
+  
+   G4LogicalVolume* logiccol9 =                         
+    new G4LogicalVolume(detcol9,         //its solid
+                        shape2_mat,          //its material
+                        "Shape2");           //its name
+               
+  new G4PVPlacement(0,                       //no rotation
+                    pos11,                    //at position
+                    logiccol9,             //its logical volume
+                    "Shape2",                //its name
+                    logicEnv,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+
+  //sphere placement
+  G4Sphere* sphere1=
+    new G4Sphere("sphere", 2*cm, 4*cm, 0*deg, 180*deg, 0*deg, 180*deg);
+
+  G4LogicalVolume* logicsphere1 =                         
+    new G4LogicalVolume(sphere1,         //its solid
+                        shape2_mat,          //its material
+                        "Sphere");           //its name
+
+    G4ThreeVector spherepos = G4ThreeVector(0*cm, 0*cm, 0*cm);
+    
+    new G4PVPlacement(0,                       //no rotation
+                    spherepos,                    //at position
+                    logicsphere1,             //its logical volume
+                    "Sphere",                //its name
+                    logicEnv,                //its mother  volume
+                    false,                   //no boolean operation
+                    0,                       //copy number
+                    checkOverlaps);          //overlaps checking
+  
   // Set Shape2 as scoring volume
   //
-  fScoringVolume = logicShape2;
+  fScoringVolume = logicdetwall;
 
   //
   //always return the physical World
